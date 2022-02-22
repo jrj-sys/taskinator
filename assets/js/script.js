ed867d97d3e66cf20dcf4719d2e7827d385bf7cd
jrj-sys <jamesjamel99@gmail.com>
2022-02-22 17:38:38 -0600

    usability changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,12 @@ var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']");
     var taskTypeInput = document.querySelector("select[name='task-type']");
-
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    // reset form after input 
+    formEl.reset();
     // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -20,15 +25,15 @@ var createTaskEl = function(taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
-    //create div to hold task info and add to list item
+    //ccreate div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
 
-    //add HTML content to div
+    // add HTML content to div
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
-    //add entire list item to list
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
