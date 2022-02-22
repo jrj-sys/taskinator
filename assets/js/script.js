de825ba046874ff752a79798f7030b12024ecdc1
jrj-sys <jamesjamel99@gmail.com>
2022-02-22 17:13:10 -0600

    capure form fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
 var formEl = document.querySelector("task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var createTaskHandler = function (event) {
     event.preventDefault();
-    
+    var taskNameInput = document.querySelector("input[name='task-name']");
+    var taskTypeInput = document.querySelector("select[name='task-type']");
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = dociment.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'> + taskNameInput + </h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
